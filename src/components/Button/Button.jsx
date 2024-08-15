199a5aed6144b7c355969c936b071390a3714d48
Rutik <rutikkulkarni2001@gmail.com>
2024-08-15 23:38:47 +0530

    Update icon field in the button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { FaShoppingCart } from "react-icons/fa";
 
-const Button = ({ text, onClick, fullWidth }) => {
+const Button = ({ text, onClick, fullWidth, icon }) => {
   return (
     <button
       onClick={onClick}
-      className={`bg-blue-500 text-white py-2 px-4 rounded ${
+      className={`flex items-center justify-center bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-6 rounded-lg transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 ${
         fullWidth ? "w-full" : ""
       }`}
     >
+      {icon && <span className="mr-2">{icon}</span>}
       {text}
     </button>
   );
